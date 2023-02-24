01034b8a8398bee6740e13cab93b071b9ee6c3c8
Kakou <525312641@qq.com>
2023-02-24 10:35:44 +0800

    asd

---
+++ src/utils/test_utils.js
@@ -0,0 +1,5 @@
+export default {
+    a: 5,
+    b: 10
+}
+export const c = 20;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import * as test from "../utils/test_utils.js";
+console.log(test, "b test");
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
