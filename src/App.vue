5870878b11b117d6521a77d21743443c25fd27a3
Hina Hikawa <hinahikawa@z100000002574.zoomlion.com>
2023-02-24 10:16:47 +0800

    99

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,6 @@
     <nav>
       <router-link to="/">Home2213</router-link> |
       <router-link to="/about">About</router-link>
-	  <router-link to="/">Home2213</router-link> |
-	  <router-link to="/about">About</router-link>
-	  <router-link to="/">Home2213</router-link> |
-	  <router-link to="/about">About</router-link>
-
     </nav>
     <router-view/>
   </div>
