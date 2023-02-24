09633e2366f10ffc91489eb334d92cbccc61bb07
Kakou <525312641@qq.com>
2023-02-24 14:38:29 +0800

    是阿四的

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home2213asdas我要杀人d</router-link> |
-      <router-link to="/about">About<git/router-link>
+      <router-link to="/">Home2213asdas我要我要杀人杀人d</router-link> |
+      <router-link to="/about">About</router-link>
     </nav>
     <router-view/>
   </div>
